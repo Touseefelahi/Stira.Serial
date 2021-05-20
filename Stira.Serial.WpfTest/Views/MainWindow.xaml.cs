b14cd657001ba2f4705ffced4492b705f270f3a3
Touseef Elahi <touseefelahi@ymail.com>
2021-05-20 14:46:11 +0300

    Add Wpf Test project

---
+++ Stira.Serial.WpfTest/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Stira.Serial.WpfTest.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
