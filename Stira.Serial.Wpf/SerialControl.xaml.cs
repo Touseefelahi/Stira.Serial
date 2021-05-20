97163e15e9cb7e8242847aef0ea27c26f08cd80c
Touseef Elahi <touseefelahi@ymail.com>
2021-05-20 14:45:25 +0300

    Add Wpf user control

---
+++ Stira.Serial.Wpf/SerialControl.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace Stira.Serial.Wpf
+{
+    /// <summary>
+    /// Interaction logic for SerialControl.xaml
+    /// </summary>
+    public partial class SerialControl : UserControl
+    {
+        public SerialControl()
+        {
+            InitializeComponent();
+            DataContext = new SerialControlViewModel();
+        }
+    }
+}
---
+++ Stira.Serial.Wpf/SerialControlViewModel.cs
@@ -0,0 +1,71 @@
+﻿using Stira.WpfCore;
+using System.Windows.Input;
+
+namespace Stira.Serial.Wpf
+{
+    public class SerialControlViewModel : BaseNotifyPropertyChanged
+    {
+        private bool isPortSelectionOpen;
+        private string toggleButtonText = "Open";
+
+        public SerialControlViewModel()
+        {
+            SerialCom = new SerialCom();
+            ToggleSerialPortCommand = new DelegateCommand(ToggleSerialPort);
+        }
+
+        public bool IsPortSelectionOpen
+        {
+            get { return isPortSelectionOpen; }
+            set
+            {
+                if (isPortSelectionOpen != value)
+                {
+                    isPortSelectionOpen = value;
+                    if (value)
+                    {
+                        OnPropertyChanged(nameof(SerialCom));
+                    }
+                    OnPropertyChanged(nameof(IsPortSelectionOpen));
+                }
+            }
+        }
+
+        public string ToggleButtonText
+        {
+            get { return toggleButtonText; }
+            set
+            {
+                if (toggleButtonText != value)
+                {
+                    toggleButtonText = value;
+                    OnPropertyChanged(nameof(ToggleButtonText));
+                }
+            }
+        }
+
+        public ISerialCom SerialCom { get; }
+
+        public ICommand ToggleSerialPortCommand { get; }
+
+        private void ToggleSerialPort()
+        {
+            try
+            {
+                if (SerialCom.IsPortOpen)
+                {
+                    SerialCom.Close();
+                }
+                else
+                {
+                    SerialCom.Open();
+                }
+                ToggleButtonText = SerialCom.IsPortOpen ? "Close" : "Open";
+                OnPropertyChanged(nameof(SerialCom));
+            }
+            catch (System.Exception)
+            {
+            }
+        }
+    }
+}
